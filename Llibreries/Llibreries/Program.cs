8072914bb14d2528712d3c546c630807e1bb8dda
Daniel Moreno Arellano <danielmorenoarellano@gmail.com>
2025-11-30 18:29:54 +0100

    Add files via upload

---
+++ Llibreries/Llibreries/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tools;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(
+            Utils.ValidateNum(10) + "\n" +
+            Utils.ValidateNum(-5) + "\n" +
+            Utils.ValidateNumPairOdd(4) + "\n" +
+            Utils.ValidateNumRange(15) + "\n" +
+            Utils.ValidatePrimeNumber(7)
+            );
+
+    }
+}
---
+++ Utils/Utils/Utils.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Tools
+{
+    public class Utils
+    {
+        // Validates that the given number is positive
+        public static string ValidateNum(int num)
+        {
+            if (num > 0)
+            {
+                return "Num valid (positive)";
+            }
+            else if (num < 0)
+            {
+                return "Num invalid (negative)";
+            }
+            else
+            {
+                return "Num is zero";
+            }
+        }
+
+        // Validates that the given number is within the specified range
+        public static string ValidateNumRange(int num, int min = 0, int max = 100)
+        {
+            if (num >= min && num <= max)
+            {
+                return $"Num valid in range {min}-{max}";
+            }
+            else
+            {
+                return $"Num invalid in range {min}-{max}";
+            }
+        }
+        public static string ValidateNumPairOdd(int num)
+        {
+            if (num % 2 == 0)
+                return $"{num} is PAIR";
+            else
+                return $"{num} is ODD";
+        }
+
+        public static string ValidatePrimeNumber(int num)
+        {
+            if (num <= 1)
+                return "Not a prime number (must be greater than 1)";
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return $"{num} is NOT prime (divisible by {i})";
+            }
+
+            return $"{num} is a PRIME number";
+        }
+
+    }
+}
